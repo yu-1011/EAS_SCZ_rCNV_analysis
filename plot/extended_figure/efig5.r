7f914bafcfd106517dc0ebdbba873d5fcef096d4
Yu Chen <normachen1995@gmail.com>
2023-12-30 22:50:05 -0500

    Update efig5.r

--- plot/extended_figure/efig5.r
+++ plot/extended_figure/efig5.r
@@ -18,17 +18,8 @@ p1 <- ggplot(data, aes(x = Status , y = as.numeric(length)/1000, fill = Status))
   geom_text(aes(x=1.5, y=max(as.numeric(length)/1000) * 1.05, label=paste0("p-value = ",signif(length_test$p.value,2)))) +
   theme_minimal()
 
-p2 <- ggplot(data, aes(x = status , y = SCORE, fill = Status)) +
-  geom_violin() +
-  #geom_jitter(width = 0.2, size = 1.5, alpha = 0.5) +
-  #scale_y_log10() +
-  labs(x = "rCNV type",
-       y = "rCNV SCORE") +
-  theme_minimal()
-wilcox.test(data[data$status=="detect","SCORE"],data[data$status=="not_detect","SCORE"])
-
 sites <- wilcox.test(data[data$status=="detect","SITES"],data[data$status=="not_detect","SITES"])
-p3 <- ggplot(data, aes(x = Status , y = SITES, fill = Status)) +
+p2 <- ggplot(data, aes(x = Status , y = SITES, fill = Status)) +
   geom_boxplot() +
   #geom_jitter(width = 0.2, size = 1.5, alpha = 0.5) +
   scale_y_log10() +
@@ -37,5 +28,5 @@ p3 <- ggplot(data, aes(x = Status , y = SITES, fill = Status)) +
   geom_text(aes(x=1.5, y=max(SITES) * 1.05, label=paste0("p-value = ",signif(sites$p.value,2)))) +
   theme_minimal()
 
-p_final <- plot_grid(p1,p3,labels = "auto")
+p_final <- plot_grid(p1,p2,labels = "auto")
 ggsave( "output/EF_05_pipeline_comarison.pdf",p_final, width=12, height=6)
